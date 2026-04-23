4a7bf9130d5eb4cd3962f035b9556355801439b6
ColsR <renner.constantin@gmail.com>
2026-04-23 11:27:18 +0200

    fix: abstraction ranking also for custom abstractions

--- src/clustering/custom_clusterer.py
+++ src/clustering/custom_clusterer.py
@@ -16,14 +16,16 @@ class CustomClusterer(AbstractClusterer):
     def build_abstractions(self, col_name):
         clusterer_entries = dict()
         clusterer_entries[f"custom{col_name}_abstracted"] = InstanceAbstraction(self.col_name, self.col_name, instance_clusterer.abstract_instance_complete, 0)
-        for level, mappings in self.reverse_map.items():
+        for level, values in self.reverse_map.items():
             # Reverse Mapping from abstracted_ value -> [specific attributes] to specific_attribute -> abstracted_value
+            ranking = values["ranking"]
+            mappings = values["hierarchy"]
             reverse_map = {
                 raw: group
                 for group, values in mappings.items()
                 for raw in values
             }
-            clusterer_entries[f"custom{col_name}_{level}"] = CustomAbstraction(col_name, col_name, reverse_map)
+            clusterer_entries[f"custom{col_name}_{level}"] = CustomAbstraction(col_name, col_name, reverse_map, ranking)
 
         clusterer_entries[f"custom{col_name}_not_abstracted"] = InstanceAbstraction(col_name, col_name, instance_clusterer.abstract_instance, 100)
         return clusterer_entries
